e8cdb790d8e30066f82c206bb1b924216c6a5f83
Moamen Eltouny <raggigroup@gmail.com>
2024-12-12 07:46:59 +0200

    Add initial project structure with README and composer configuration

---
+++ src/AssistantServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MoamenEltouny\Laravel\Assistant;
+
+use Illuminate\Support\ServiceProvider;
+
+class AssistantServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // 
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 
+    }
+}
