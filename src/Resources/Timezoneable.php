010441808c4229140ce6d38ddd003e15ac672512
Moamen Eltouny <raggigroup@gmail.com>
2024-12-28 17:38:19 +0200

    Add Timezoneable trait for handling datetime in user timezones

---
+++ src/Resources/Timezoneable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Pharaonic\Laravel\Assistant\Resources;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * @method string|\Carbon\Carbon timezone(string $attribute, string|null $format = 'Y-m-d H:i', string $action = 'format', Authenticatable|null $user = null)
+ */
+trait Timezoneable
+{
+    /**
+     * Get the datetime in the user's timezone.
+     *
+     * @param string $attribute
+     * @param string|null $format
+     * @param string $action
+     * @param Authenticatable|null $user
+     * @return string|\Carbon\Carbon
+     */
+    public function timezone(string $attribute, string|null $format = 'Y-m-d H:i', string $action = 'format', Authenticatable $user = null)
+    {
+        $dt = $this->{$attribute}->timezone(($user ?? Auth::user())?->timezone ?? 'UTC');
+
+        if ($format === null) {
+            return $dt;
+        }
+
+        return $dt->{$action}($format);
+    }
+}
