148d2366d36c579594c5d783aad2d206fb54503d
Moamen Eltouny <raggigroup@gmail.com>
2025-08-24 13:43:40 +0300

    Refactor FileableResourceMixin methods to improve thumbnail handling and streamline file information retrieval

--- src/Http/Resources/Json/FileableResourceMixin.php
+++ src/Http/Resources/Json/FileableResourceMixin.php
@@ -6,71 +6,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class FileableResourceMixin
 {
-    /**
-     * Get the file information.
-     *
-     * @param  string       $name
-     * @param  string       $target
-     * @param  boolean      $isTemporary
-     * @param  integer|null $expire
-     * @param  Model|null   $resource
-     * @return array|null
-     */
-    public function file(string $name, string $target = 'info', bool $isTemporary = false, ?int $expire = null, ?Model $resource = null)
+    public function thumbnail()
     {
-        $resource ??= $this->resource;
+        return function (string $name, string $target = 'info', bool $isTemporary = false, ?int $expire = null, ?Model $resource = null) {
+            $resource ??= $this->{'resource'};
 
-        if (!$resource->isRelation('files') || !$resource->relationLoaded('files') || !$resource->{$name}) {
-            return null;
-        }
+            if (!$resource->isRelation('files') || !$resource->relationLoaded('files') || !$resource->{$name} || !$resource->{$name}->upload?->thumbnail_id) {
+                return null;
+            }
 
-        return $resource->{$name}->upload?->info($target, $isTemporary, $expire);
+            return $resource->{$name}->upload?->thumbnail?->info($target, $isTemporary, $expire);
+        };
     }
 
-    /**
-     * Get the thumbnail information.
-     *
-     * @param  string       $name
-     * @param  string       $target
-     * @param  boolean      $isTemporary
-     * @param  integer|null $expire
-     * @param  Model|null   $resource
-     * @return array|null
-     */
-    public function thumbnail(string $name, string $target = 'info', bool $isTemporary = false, ?int $expire = null, ?Model $resource = null)
+    public function file()
     {
-        $resource ??= $this->resource;
+        return function (string $name, string $target = 'info', bool $isTemporary = false, ?int $expire = null, ?Model $resource = null) {
+            $resource ??= $this->{'resource'};
 
-        if (!$resource->isRelation('files') || !$resource->relationLoaded('files') || !$resource->{$name} || !$resource->{$name}->upload?->thumbnail_id) {
-            return null;
-        }
+            if (!$resource->isRelation('files') || !$resource->relationLoaded('files') || !$resource->{$name}) {
+                return null;
+            }
 
-        return $resource->{$name}->upload?->thumbnail?->info($target, $isTemporary, $expire);
+            return $resource->{$name}->upload?->info($target, $isTemporary, $expire);
+        };
     }
 
-    /**
-     * Get all files information.
-     *
-     * @param  string       $target
-     * @param  boolean      $isTemporary
-     * @param  integer|null $expire
-     * @param  Model|null   $resource
-     * @return array|null
-     */
-    public function files(string $target = 'info', bool $isTemporary = false, ?int $expire = null, ?Model $resource = null)
+    public function files()
     {
-        $resource ??= $this->resource;
+        return function (string $target = 'info', bool $isTemporary = false, ?int $expire = null, ?Model $resource = null) {
+            $resource ??= $this->{'resource'};
 
-        if (!$resource->isRelation('files') || !$resource->relationLoaded('files')) {
-            return null;
-        }
+            if (!$resource->isRelation('files') || !$resource->relationLoaded('files')) {
+                return null;
+            }
 
-        return $resource->files
-            ->mapWithKeys(function ($file) use ($target, $isTemporary, $expire) {
-                return [
-                    $file->field => $file->upload?->info($target, $isTemporary, $expire)
-                ];
-            })
-            ->toArray();
+            return $resource->files
+                ->mapWithKeys(function ($file) use ($target, $isTemporary, $expire) {
+                    return [
+                        $file->field => $file->upload?->info($target, $isTemporary, $expire)
+                    ];
+                })
+                ->toArray();
+        };
     }
 }
