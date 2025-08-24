fe391d92483c43f3a6727970ac194002c1835340
Moamen Eltouny <raggigroup@gmail.com>
2025-08-24 13:43:46 +0300

    Refactor datetime method in TimeableResourceMixin to return a closure for improved flexibility in handling datetime attributes

--- src/Http/Resources/Json/TimeableResourceMixin.php
+++ src/Http/Resources/Json/TimeableResourceMixin.php
@@ -6,17 +6,23 @@ use Illuminate\Contracts\Auth\Authenticatable;
 
 class TimeableResourceMixin
 {
-    /**
-     * Get the datetime in the user's timezone with the specified format and action.
-     *
-     * @param string $attribute
-     * @param string|null $format
-     * @param string|null $action
-     * @param Authenticatable|null $user
-     * @return array|null
-     */
-    public function datetime(string $attribute, ?string $format, ?string $action, ?Authenticatable $user, $isHijri = false)
+    public function datetime()
     {
-        return datetime($this->resource, $attribute, $format, $action, $user, $isHijri);
+        return function (
+            string $attribute,
+            ?string $format = null,
+            ?string $action = null,
+            ?Authenticatable $user = null,
+            $isHijri = false
+        ) {
+            return datetime(
+                $this->{'resource'},
+                $attribute,
+                $format,
+                $action,
+                $user,
+                $isHijri
+            );
+        };
     }
 }
